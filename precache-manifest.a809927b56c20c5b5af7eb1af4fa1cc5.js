a20c2b7e6687bf1ee564e108a4a0d4599fab02cc
n-bhasin <nkbhasin28@gmail.com>
2020-06-07 15:30:55 -0400

    Updates

---
+++ precache-manifest.a809927b56c20c5b5af7eb1af4fa1cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707c4e9f6f4672025212e19572a5b830",
+    "url": "/SImpleIPFS/index.html"
+  },
+  {
+    "revision": "29f0bcdd887a68cd5372",
+    "url": "/SImpleIPFS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b197e56678424f35be87",
+    "url": "/SImpleIPFS/static/js/2.c4e90714.chunk.js"
+  },
+  {
+    "revision": "78d7a16074ae49492667f04783c80fc6",
+    "url": "/SImpleIPFS/static/js/2.c4e90714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f0bcdd887a68cd5372",
+    "url": "/SImpleIPFS/static/js/main.7f8c4b22.chunk.js"
+  },
+  {
+    "revision": "3d3f8444e63f273cbbb3",
+    "url": "/SImpleIPFS/static/js/runtime-main.d2ec8b2f.js"
+  }
+]);
